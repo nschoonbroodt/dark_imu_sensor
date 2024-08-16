5dd711fb0636ca016a3d832d608b663fc0b385a8
Nicolas Schoonbroodt <nicolas@elsewhere.be>
2024-08-17 00:03:04 +0200

    adding period parameter with callback to change the timer

--- imu_sensor/src/imu_sensor.cpp
+++ imu_sensor/src/imu_sensor.cpp
@@ -11,7 +11,6 @@
 using namespace std::chrono_literals;
 
 IMUSensor::IMUSensor() : Node("imu_sensor") {
-
     // Subscribe to trajectory to remember the last 2 points
     this->subscription_ = 
         this->create_subscription<imu_sensor::msg::Trajectory>(
@@ -23,10 +22,35 @@ IMUSensor::IMUSensor() : Node("imu_sensor") {
     
     // Register a timer (for the moment at constant speed) to publish the IMU Data
     publisher_ = this->create_publisher<imu_sensor::msg::IMUData>("imu_data", 10);
-    timer_ = this->create_wall_timer(10ms, [this]() {
+    this->create_timer();
+
+    // declare parameters
+    // To react on parameter change
+    param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
+    
+    // Period of message sending
+    auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+    param_desc.description = "The period between two IMU Sensor message, in ms";
+    this->declare_parameter("imu_message_period", 10, param_desc);
+    this->message_frequency_ = 10 * 1ms;
+    // TODO: I have 10ms hardcoded in 2 places! With 2 different units
+    auto periodCb = [this](const rclcpp::Parameter &period) {
+        std::chrono::milliseconds new_period = period.as_int() * 1ms;
+        if (this->message_frequency_ == new_period) {
+            return; // no changes
+        }
+        this->message_frequency_ = new_period;
+        this->create_timer();
+        // TODO: maybe check allowed timer values
+    };
+    this->cb_handle_ = param_subscriber_->add_parameter_callback("imu_message_period", periodCb);
+};
+
+void IMUSensor::create_timer() {
+    timer_ = this->create_wall_timer(message_frequency_, [this]() {
         this->imu_message_send();
     });
-};
+}
 
 void IMUSensor::trajectory_callback(const imu_sensor::msg::Trajectory & trajectory) {
     this->secondLastTrajectory_ = this->lastTrajectory_;
--- imu_sensor/src/imu_sensor.hpp
+++ imu_sensor/src/imu_sensor.hpp
@@ -1,6 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "imu_sensor/msg/trajectory.hpp"
 
+#include <chrono>
+
 class IMUSensor : public rclcpp::Node {
 public:
     IMUSensor();
@@ -11,8 +13,15 @@ private:
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<imu_sensor::msg::IMUData>::SharedPtr publisher_;
 
+    std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
+    std::shared_ptr<rclcpp::ParameterCallbackHandle> cb_handle_;
+
+    // current (last) frequency of update
+    std::chrono::milliseconds message_frequency_;
+
     void trajectory_callback(const imu_sensor::msg::Trajectory & trajectory);
     void imu_message_send();
+
+    void create_timer();
+
 };
-
-
